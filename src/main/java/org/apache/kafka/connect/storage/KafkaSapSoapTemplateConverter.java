4452d83ac75e93d19dc90df61af3dd44b1ae1456
Marcel Boldt <mboldt@confluent.io>
2023-08-16 16:10:48 +0200

    Works, with StringSubstitutor

--- src/main/java/org/apache/kafka/connect/storage/KafkaSapSoapTemplateConverter.java
+++ src/main/java/org/apache/kafka/connect/storage/KafkaSapSoapTemplateConverter.java
@@ -1,2 +1,90 @@
-package org.apache.kafka.connect.storage.kafkaSapSoapTemplateConverter;public class kafkaSapSoapTemplateConverter {
+package org.apache.kafka.connect.storage;
+
+import org.apache.commons.text.StringSubstitutor;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaAndValue;
+import org.apache.kafka.connect.errors.DataException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+
+public class KafkaSapSoapTemplateConverter extends StringConverter{
+
+  private static final Logger log = LoggerFactory.getLogger(KafkaSapSoapTemplateConverter.class);
+
+
+  private final StringDeserializer deserializer = new StringDeserializer();
+
+  StringWriter sw = new StringWriter();
+  String template;
+  Map<String, String> valuesMap;
+
+
+
+  @Override
+  public void configure(Map<String, ?> configs) {
+    super.configure(configs);
+
+    KafkaSapSoapTemplateConverterConfig conf = new KafkaSapSoapTemplateConverterConfig(configs);
+
+    valuesMap = new HashMap<String, String>();
+    valuesMap.put("login_name", conf.sap_userid());
+    valuesMap.put("login_password", conf.sap_password());
+
+    ClassLoader classLoader = getClass().getClassLoader();
+    InputStream inputStream = classLoader.getResourceAsStream(conf.sap_xml_url());
+    try {
+      template = readFromInputStream(inputStream);
+    } catch (Exception e) {
+      log.warn("Failed to deserialize string: ", e.toString());
+    }
+
+  }
+
+  @Override
+  public SchemaAndValue toConnectData(String topic, byte[] value) {
+    try {
+      return new SchemaAndValue(Schema.OPTIONAL_STRING_SCHEMA, stringReplace(deserializer.deserialize(topic, value)));
+    } catch (SerializationException e) {
+      throw new DataException("Failed to deserialize string: ", e);
+    }
+  }
+
+
+  private String readFromInputStream(InputStream inputStream)
+      throws IOException {
+    StringBuilder resultStringBuilder = new StringBuilder();
+    try (BufferedReader br
+             = new BufferedReader(new InputStreamReader(inputStream))) {
+      String line;
+      while ((line = br.readLine()) != null) {
+        resultStringBuilder.append(line).append("\n");
+      }
+    }
+    return resultStringBuilder.toString();
+  }
+
+  public String stringReplace(String message) {
+
+    valuesMap.put("message", message);
+    //log.info("Values: " + valuesMap.toString());
+    StringSubstitutor sub = new StringSubstitutor(valuesMap);
+    String substituted = sub.replace(template);
+    //log.info("Substituted: " + substituted);
+
+    return substituted;
+  }
 }
--- src/main/java/org/apache/kafka/connect/storage/KafkaSapSoapTemplateConverterConfig.java
+++ src/main/java/org/apache/kafka/connect/storage/KafkaSapSoapTemplateConverterConfig.java
@@ -1,2 +1,67 @@
-package org.apache.kafka.connect.storage;public class KafkaSapSoapTemplateConverterConfig {
+package org.apache.kafka.connect.storage;
+
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.common.config.ConfigDef.Importance;
+import org.apache.kafka.common.config.ConfigDef.Type;
+import org.apache.kafka.common.config.ConfigDef.Width;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+public class KafkaSapSoapTemplateConverterConfig extends ConverterConfig {
+
+  public static final String ENCODING_CONFIG = "converter.encoding";
+  public static final String ENCODING_DEFAULT = StandardCharsets.UTF_8.name();
+  private static final String ENCODING_DOC = "The name of the Java character set to use for encoding strings as byte arrays.";
+  private static final String ENCODING_DISPLAY = "Encoding";
+
+  public static final String SAP_LOGIN_USER_CONFIG = "sap.login.user";
+  public static final String SAP_LOGIN_USER_DEFAULT = "kafka";
+  public static final String SAP_LOGIN_USER_DOC = "The user id for SAP MII basic auth.";
+  public static final String SAP_LOGIN_USER_DISPLAY = "SAP user id";
+
+  public static final String SAP_LOGIN_PASSWORD_CONFIG = "sap.login.password";
+  public static final String SAP_LOGIN_PASSWORD_DEFAULT = "confluent";
+  public static final String SAP_LOGIN_PASSWORD_DOC = "The password for SAP MII basic auth.";
+  public static final String SAP_LOGIN_PASSWORD_DISPLAY = "SAP user password";
+
+  public static final String SAP_XML_TEMPLATE_LOCATION_CONFIG = "sap.xml.template.location";
+  public static final String SAP_XML_TEMPLATE_LOCATION_DEFAULT = "template/kafkaSapXmlTemplate.xml";
+  public static final String SAP_XML_TEMPLATE_LOCATION_DOC = "The Velocity xml location URL.";
+  public static final String SAP_XML_TEMPLATE_LOCATION_DISPLAY = "Xml template URL";
+
+  private final static ConfigDef CONFIG;
+
+  static {
+    CONFIG = ConverterConfig.newConfigDef();
+    CONFIG.define(ENCODING_CONFIG, Type.STRING, ENCODING_DEFAULT, Importance.HIGH, ENCODING_DOC, null, -1, Width.MEDIUM,
+        ENCODING_DISPLAY);
+    CONFIG.define(SAP_LOGIN_USER_CONFIG, Type.STRING, SAP_LOGIN_USER_DEFAULT, Importance.HIGH, SAP_LOGIN_USER_DOC,
+        null, -1, Width.MEDIUM, SAP_LOGIN_USER_DISPLAY);
+    CONFIG.define(SAP_LOGIN_PASSWORD_CONFIG, Type.STRING, SAP_LOGIN_PASSWORD_DEFAULT, Importance.HIGH, SAP_LOGIN_PASSWORD_DOC,
+        null, -1, Width.MEDIUM, SAP_LOGIN_PASSWORD_DISPLAY);
+    CONFIG.define(SAP_XML_TEMPLATE_LOCATION_CONFIG, Type.STRING, SAP_XML_TEMPLATE_LOCATION_DEFAULT, Importance.HIGH, SAP_XML_TEMPLATE_LOCATION_DOC,
+        null, -1, Width.MEDIUM, SAP_XML_TEMPLATE_LOCATION_DISPLAY);
+  }
+
+  public static ConfigDef configDef() {
+    return CONFIG;
+  }
+
+  public KafkaSapSoapTemplateConverterConfig(Map<String, ?> props) {
+    super(CONFIG, props);
+  }
+
+  public String sap_userid() { return getString(SAP_LOGIN_USER_CONFIG); }
+
+  public String sap_password() { return getString(SAP_LOGIN_PASSWORD_CONFIG); }
+
+  public String sap_xml_url() {
+    return getString(SAP_XML_TEMPLATE_LOCATION_CONFIG);
+  }
+
+  public String encoding() {
+    return getString(ENCODING_CONFIG);
+  }
+
 }
